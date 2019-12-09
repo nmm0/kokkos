1775637d4b7a177f352b95dfcf49793efc099ba3
jeffmiles63 <jsmiles@apollo.sandia.gov>
2019-12-09 14:30:03 -0700

    update combination logic

--- benchmarks/benchmark_suite/scripts/run_tests.bash
+++ benchmarks/benchmark_suite/scripts/run_tests.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LOOP_REPEAT=1
+if [ "$1" != "" ]; then
+   LOOP_REPEAT=$1
+   echo Repeat loop = $LOOP_REPEAT
+fi
+
 # BytesAndFlops
 pushd ./build/bytes_and_flops
 
@@ -19,48 +25,44 @@ BAF_PERF_2=`./${BAF_EXE} 2 100000 1024 16 1 8 64 ${TEAM_SIZE} 6000 | awk '{print
 echo "BytesAndFlops: ${BAF_PERF_1} ${BAF_PERF_2}"
 popd
 
-read -rsp $'Press any key to continue...\n' -n 1 key
-
 # MiniMD
 pushd ./build/miniMD
 rm minimd.1
 rm minimd.2
 cp ../../miniMD/kokkos/Cu_u6.eam ./
-for i in {1..5}
+for i in $( seq 0 ${LOOP_REPEAT} )
 do
-  MD_PERF_1=`mpiexec -n 1 ./miniMD --half_neigh 0 -s 60 --ntypes 1 -t ${OMP_NUM_THREADS} -i ../../miniMD/kokkos/in.eam.miniMD | grep PERF_SUMMARY | awk '{print $10/21163341}'`
-  MD_PERF_2=`mpiexec -n 1 ./miniMD --half_neigh 0 -s 20 --ntypes 1 -t ${OMP_NUM_THREADS} -i ../../miniMD/kokkos/in.eam.miniMD | grep PERF_SUMMARY | awk '{print $10/13393417}'`
+  mpiexec -n 1 ./miniMD --half_neigh 0 -s 60 --ntypes 1 -t ${OMP_NUM_THREADS} -i ../../miniMD/kokkos/in.eam.miniMD > md_perf1.out
+  mpiexec -n 1 ./miniMD --half_neigh 0 -s 20 --ntypes 1 -t ${OMP_NUM_THREADS} -i ../../miniMD/kokkos/in.eam.miniMD  > md_perf2.out
 
-  echo ${MD_PERF_1} >> minimd.1
-  echo ${MD_PERF_2} >> minimd.2
+  cat md_perf1.out | grep PERF_SUMMARY | awk '{print $10/21163341}' >> minimd.1
+  cat md_perf2.out | grep PERF_SUMMARY | awk '{print $10/13393417}' >> minimd.2
 done
 TOT_PERF_1=0
 let loop_cnt=0
 while read -r result_one; do
-   TOT_PERF_1= `echo "${TOT_PERF_1} | ${result_one}" | bc -l`
+   TOT_PERF_1=$(echo ${TOT_PERF_1} + ${result_one} | bc)
    let loop_cnt+=1
 done < ./minimd.1
 
-MD_AVG_PERF_1=`echo "${TOT_PERF_1} / ${loop_cnt}" | bc -l`
+MD_AVG_PERF_1=$(echo "scale=7; ${TOT_PERF_1} / ${loop_cnt}" | bc)
 
 TOT_PERF_2=0
 let loop_cnt=0
 while read -r result_one; do
-   TOT_PERF_2= `echo "${TOT_PERF_2} | ${result_one}" | bc -l`
+   TOT_PERF_2=$(echo ${TOT_PERF_2} + ${result_one} | bc)
    let loop_cnt+=1
 done < ./minimd.2
 
-MD_AVG_PERF_2=`echo "${TOT_PERF_2} / ${loop_cnt}" | bc -l`
-echo "miniMD: ${MD_AVG_PERF_1} ${MD_AVG_PERF_2}"
+MD_AVG_PERF_2=$(echo "scale=7; ${TOT_PERF_2} / ${loop_cnt}" | bc)
+echo "miniMD: ${MD_AVG_PERF_1} ${MD_AVG_PERF_2} ${loop_cnt}"
 popd
-
-read -rsp $'Press any key to continue...\n' -n 1 key
 
 # MiniFE
 pushd ./build/miniFE
 rm minife.1
 rm minife.2
-for i in {1..5}
+for i in $( seq 0 ${LOOP_REPEAT} )
 do
   rm *.yaml
   ./miniFE.x -nx 100 &> /dev/null
@@ -74,25 +76,23 @@ done
 TOT_PERF_1=0
 let loop_cnt=0
 while read -r result_one; do
-   TOT_PERF_1= `echo "${TOT_PERF_1} | ${result_one}" | bc -l`
+   TOT_PERF_1=$(echo ${TOT_PERF_1} + ${result_one} | bc)
    let loop_cnt+=1
 done < ./minife.1
 
-FE_AVG_PERF_1=`echo "${TOT_PERF_1} / ${loop_cnt}" | bc -l`
+FE_AVG_PERF_1=$(echo "scale=7; ${TOT_PERF_1} / ${loop_cnt}" | bc)
 
 TOT_PERF_2=0
 let loop_cnt=0
 while read -r result_one; do
-   TOT_PERF_2= `echo "${TOT_PERF_2} | ${result_one}" | bc -l`
+   TOT_PERF_2=$(echo ${TOT_PERF_2} + ${result_one} | bc)
    let loop_cnt+=1
 done < ./minife.2
 
-FE_AVG_PERF_2=`echo "${TOT_PERF_2} / ${loop_cnt}" | bc -l`
-echo "MiniFE: ${FE_PERF_1} ${FE_PERF_2}"
+FE_AVG_PERF_2=$(echo "scale=7; ${TOT_PERF_2} / ${loop_cnt}" | bc)
+echo "MiniFE: ${FE_AVG_PERF_1} ${FE_AVG_PERF_2} ${loop_cnt}"
 
 popd
-
-read -rsp $'Press any key to continue...\n' -n 1 key
 
 PERF_RESULT=`echo "${BAF_PERF_1} ${BAF_PERF_2} ${MD_AVG_PERF_1} ${MD_AVG_PERF_2} ${FE_AVG_PERF_1} ${FE_AVG_PERF_2}" | awk '{print ($1+$2+$3+$4+$5+$6)/6}'`
 echo "Total Result: " ${PERF_RESULT}
